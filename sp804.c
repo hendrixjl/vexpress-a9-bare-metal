ac2e1390617b92d369a7677c6c7cc05e39cdc509
John Hendrix <hendrix.john64@gmail.com>
2022-04-26 10:26:45 -0500

    Two timers

--- sp804.c
+++ sp804.c
@@ -3,21 +3,22 @@
 
 volatile timer804_t* const tregs = (timer804_t*)TIMER_BASE;
 
-volatile uint32_t counter;
-
-void timer_handler(void){
-	tregs->timers[0].IntClr = 0;
-}
 
 void timer_init(void){
-    counter = 0;
-    tregs->timers[0].Control = SP804_TIMER_PERIODIC | SP804_TIMER_32BIT  | SP804_TIMER_PRESCALE_256; // | SP804_TIMER_INT_ENABLE;
+    tregs->timers[0].Control = SP804_TIMER_PERIODIC | SP804_TIMER_32BIT  | SP804_TIMER_PRESCALE_256; 
     tregs->timers[0].Load = 0;
     tregs->timers[0].Value = 0;
     tregs->timers[0].RIS = 0;
     tregs->timers[0].MIS = 0;
     tregs->timers[0].BGLoad = 0x10000;
     tregs->timers[0].Control |= SP804_TIMER_ENABLE;
-    //install_isr(TIM01INT_IRQn,timer_handler);
-    //enable_irq(TIM01INT_IRQn);
+
+    tregs->timers[1].Control = SP804_TIMER_FREE_RUNNING | SP804_TIMER_32BIT  | SP804_TIMER_PRESCALE_256; 
+    tregs->timers[1].Load = 0;
+    tregs->timers[1].Value = 0;
+    tregs->timers[1].RIS = 0;
+    tregs->timers[1].MIS = 0;
+    tregs->timers[1].BGLoad = 0;
+    tregs->timers[1].Control |= SP804_TIMER_ENABLE;
+
 }
--- sp804.h
+++ sp804.h
@@ -3,13 +3,15 @@
 #include <stdint.h>
 /* Flags for the timer control registers  */
 #define SP804_TIMER_ENABLE       (1 << 7)
+#define SP804_TIMER_FREE_RUNNING (0 << 6)
 #define SP804_TIMER_PERIODIC     (1 << 6)
 #define SP804_TIMER_INT_ENABLE   (1 << 5)
 #define SP804_TIMER_PRESCALE_1   (0 << 2)
 #define SP804_TIMER_PRESCALE_16  (1 << 2)
 #define SP804_TIMER_PRESCALE_256 (2 << 2)
 #define SP804_TIMER_32BIT        (1 << 1)
-#define SP804_TIMER_ONESHOT (1 << 0)
+#define SP804_TIMER_16BIT        (0 << 1)
+#define SP804_TIMER_ONESHOT      (1 << 0)
 
 #define TIMER_BASE 0x10011000
 
